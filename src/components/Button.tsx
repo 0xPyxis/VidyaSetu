f75ade8b0035681753152bb3c619a1fbfa703dbe
Anushree R <anushreer695@gmail.com>
2026-06-06 19:27:56 +0530

    resolve merge conflicts: fixed Button action type and replaced empty string actions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,16 +10,16 @@ function Button({
   hover = '',
 }: {
   text: string;
-  action: () => void;
-  color: string;
-  textCol: string;
-  additional: string;
-  hover: string;
+  action?: () => void;
+  color?: string;
+  textCol?: string;
+  additional?: string;
+  hover?: string;
 }) {
   return (
     <button
       className={` ${color ? color : 'bg-primary'} ${textCol ? textCol : ' text-white'} ${additional ? additional : ''} ${hover ? hover : 'hover:bg-primary/60'} uppercase   p-4 pl-8 pr-8   font-bold  transition-all duration-300 cursor-pointer text-[14px]`}
-      onClick={action}
+      onClick={typeof action === 'function' ? action : undefined}
     >
       {text}
     </button>
